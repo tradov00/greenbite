1446ac4143493fe544d5cbc50835400f161275ad
tradov00 <tradov00@fesb.hr>
2025-05-24 17:38:27 +0200

    minor fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,17 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { UserProvider } from "@/contexts/UserContext";
+import type { ReactNode } from "react";
 
 export const metadata = {
   title: "GreenBite",
   description: "Eco-friendly recipes for a better world",
-    icons: {
-    icon: '/favicon.ico',
+  icons: {
+    icon: "/favicon.ico",
   },
-
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
